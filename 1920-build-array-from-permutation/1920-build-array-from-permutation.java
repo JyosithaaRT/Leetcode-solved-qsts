10c4040824a32c597f12153f8dc46782e1401435
Jyosithaa <jyosithaaramesh@gmail.com>
2026-08-04 20:35:22 +0530

    Time: 1 ms (99.49%), Space: 47.2 MB (33.02%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length]; // create new array
+        int[] ans = new int[nums.length]; 
         for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[nums[i]];       // core logic
+            ans[i] = nums[nums[i]];      
         }
         return ans;
     }
